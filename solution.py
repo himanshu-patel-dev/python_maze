7e90704e87a1a9fac3149689313a2bb8cc50a800
himanshu.patel <himanshu.patel@clarisights.com>
2024-10-04 11:43:18 +0530

    add one test case

--- solution.py
+++ solution.py
@@ -6,6 +6,7 @@ class Solution:
   #   ['1', '0', '0', '0'],
   #   ['1', '1', '0', 'E']
 	# ]
+	# start_position = (0,0)
 	# return: Array of tuples [ (0,1),(1,1) ]
-	def get_path(self, maze):
+	def get_path(self, maze, start_position):
 		return [(0,0)]
